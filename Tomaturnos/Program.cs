9905dbe1dbb5443c4ea54304f7d7fdb26aa0b51c
Armando Terán Castillo <armandoterancastillo1@outlook.com>
2025-04-11 14:58:17 -0600

    ajuste en program

--- Tomaturnos/Program.cs
+++ Tomaturnos/Program.cs
@@ -1,11 +1,39 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TomaTurnos.Data.Dependencies.Services;
+using TomaTurnos.Data.Dependencies.Services.Requests;
+using TomaTurnos.Data.Dependencies.Transients;
 using Tomaturnos;
+using TomaTurnos.Data.Dependencies;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Configurar root components
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Configurar HttpClient
+builder.Services.AddScoped(_ => new HttpClient 
+{ 
+    BaseAddress = new Uri(Variables.GetApiUrl()) 
+});
+
+// Registrar servicios
+builder.Services.AddScoped<ISignalRService, SignalRService>();
+builder.Services.AddScoped<MunicipioState>();
+
+// Registrar servicios personalizados
+builder.Services.AddScoped<IHttpService, HttpService>(sp => 
+{
+    HttpClient httpClient = sp.GetRequiredService<HttpClient>();
+    TurnosService turnosService = new(httpClient);
+    return new HttpService(httpClient, turnosService);
+});
+
+builder.Services.AddScoped<ITurnosService, TurnosService>(sp => 
+{
+    HttpClient httpClient = sp.GetRequiredService<HttpClient>();
+    return new TurnosService(httpClient);
+});
 
 await builder.Build().RunAsync();
