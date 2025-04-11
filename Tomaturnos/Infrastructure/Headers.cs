8221ad2606ccb5d6c9b3d7086eb8ef9494af0c2d
Armando Terán Castillo <armandoterancastillo1@outlook.com>
2025-04-11 16:23:10 -0600

    Update Headers.cs

--- Tomaturnos/Infrastructure/Headers.cs
+++ Tomaturnos/Infrastructure/Headers.cs
@@ -6,10 +6,5 @@
         {
             { "token", " $1voEd9dER$9/srvCs2bijyRFSJtjXQp0" },
         };
-        
-        public static readonly Dictionary<string, string> SecondApiHeaders = new()
-        {
-            { "X-API-KEY", "9f6a9dbf-2741-4efc-9316-76bde7ceed0f" }, 
-        };
     }
 }
